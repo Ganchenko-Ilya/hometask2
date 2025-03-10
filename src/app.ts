d665218fd9f024d8d973e5291ba16ac44320e436
Ganchenko-Ilya <chicha3d@gmail.com>
2025-03-11 02:44:09 +0300

    fix1

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { blogsRouters } from './routes/blogs-routers';
 import { postsRouters } from './routes/posts-routers';
+import { deleteAll } from './testing/deleteAll';
 
 const cors = require('cors');
 export const app = express();
@@ -9,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/blogs', blogsRouters);
 app.use('/posts', postsRouters);
-app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', "default-src 'self'; connect-src 'self' http://localhost:3003");
-  next();
+
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+  deleteAll();
 });
