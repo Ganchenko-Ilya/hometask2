5554ad658cbd53ab4427b1759d70f61012c25bb5
Ganchenko-Ilya <chicha3d@gmail.com>
2025-03-30 15:41:16 +0300

    fix20

--- src/middlewairs/validate-middleawairs/query-validators/query-validators.ts
+++ src/middlewairs/validate-middleawairs/query-validators/query-validators.ts
@@ -20,51 +20,51 @@ const pageSizeValidator = query('pageSize')
   .custom((value) => !!+value || !value)
   .withMessage('pageSize shout be is  number type');
 
-// const sortByValidator = query('sortBy')
-//   .trim()
-//   .custom((value, { req }) => {
-//     const sortDirection = req.query?.sortDirection;
-//     const isArraySortDirection = Array.isArray(sortDirection);
-//     console.log(isArraySortDirection);
-//     console.log(!value);
-//     if (!value || !Array.isArray(value)) {
-//       if (!isArraySortDirection) {
-//         return true;
-//       } else {
-//         return false;
-//       }
-//     }
-//     if (Array.isArray(value)) {
-//       if (isArraySortDirection) {
-//         if (sortDirection.length > value.length) {
-//           return false;
-//         } else {
-//           return true;
-//         }
-//       } else {
-//         return true;
-//       }
-//     } else if (isArraySortDirection) {
-//       return false;
-//     }
-//   })
-//   .withMessage(
-//     'sortBy shout be string or array with  Blog item types that  access ' +
-//       'for sorting and sortBy Array shout not be less sortDirection Array  ',
-//   );
-
-const sortDirectionValidator = query('sortDirection')
+const sortByValidator = query('sortBy')
   .trim()
-  .custom((value) => {
-    if (!value) {
-      return true;
+  .custom((value, { req }) => {
+    const sortDirection = req.query?.sortDirection;
+    const isArraySortDirection = Array.isArray(sortDirection);
+    console.log(isArraySortDirection);
+    console.log(!value);
+    if (!value || !Array.isArray(value)) {
+      if (!isArraySortDirection) {
+        return true;
+      } else {
+        return false;
+      }
     }
     if (Array.isArray(value)) {
-      return value.every((el) => accessDirections.includes(el));
+      if (isArraySortDirection) {
+        if (sortDirection.length > value.length) {
+          return false;
+        } else {
+          return true;
+        }
+      } else {
+        return true;
+      }
+    } else if (isArraySortDirection) {
+      return false;
     }
-    return false;
   })
-  .withMessage('sortDirection shout be string or array with types  asc | desc');
+  .withMessage(
+    'sortBy shout be string or array with  Blog item types that  access ' +
+      'for sorting and sortBy Array shout not be less sortDirection Array  ',
+  );
+
+// const sortDirectionValidator = query('sortDirection')
+//   .trim()
+//   .custom((value) => {
+//     if (!value) {
+//       return true;
+//     }
+//     if (Array.isArray(value)) {
+//       return value.every((el) => accessDirections.includes(el));
+//     }
+//     return false;
+//   })
+//   .withMessage('sortDirection shout be string or array with types  asc | desc');
 
 const searchNameTermValidator = query('searchNameTerm')
   .trim()
@@ -73,8 +73,8 @@ const searchNameTermValidator = query('searchNameTerm')
 export const queryValidators = [
   pageNumberValidator,
   pageSizeValidator,
-  // sortByValidator,
-  sortDirectionValidator,
+  sortByValidator,
+  // sortDirectionValidator,
   searchNameTermValidator,
   handlerErrorsValidator,
 ];
